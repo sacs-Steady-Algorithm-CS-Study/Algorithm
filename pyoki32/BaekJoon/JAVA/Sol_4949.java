b5504b87270a86f652fe297696e91f914852b7bd
pyoki32 <pyoki32@gmail.com>
2024-01-14 16:12:46 +0900

    Baekjoon Sol_4949 code convention

--- pyoki32/BaekJoon/JAVA/Sol_4949.java
+++ pyoki32/BaekJoon/JAVA/Sol_4949.java
@@ -9,37 +9,36 @@ public class Sol_4949 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        while(true){
+        while (true) {
             String str = br.readLine();
-            if(str.charAt(0) == '.'){
+            if (str.charAt(0) == '.') {
                 break;
             }
             boolean balanceCheck = true;
             Stack<Character> stack = new Stack<>();
-            for(int i= 0;i<str.length();i++){
-                if(str.charAt(i) == '(' || str.charAt(i) == '['){
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '(' || str.charAt(i) == '[') {
                     stack.add(str.charAt(i));
-                }else if(str.charAt(i) == ')' || str.charAt(i) == ']'){
-                    if(!stack.isEmpty()){
-                        if((stack.peek() == '(' && str.charAt(i) == ')') ||(stack.peek() == '[' && str.charAt(i) == ']') ){
+                } else if (str.charAt(i) == ')' || str.charAt(i) == ']') {
+                    if (!stack.isEmpty()) {
+                        if ((stack.peek() == '(' && str.charAt(i) == ')') || (stack.peek() == '[' && str.charAt(i) == ']')) {
                             stack.pop();
-                        }else{
+                        } else {
                             balanceCheck = false;
                             break;
                         }
-                    }
-                    else{
+                    } else {
                         balanceCheck = false;
                         break;
                     }
                 }
             }
-            if(!stack.isEmpty()){
-                balanceCheck =  false;
+            if (!stack.isEmpty()) {
+                balanceCheck = false;
             }
-            if(balanceCheck){
+            if (balanceCheck) {
                 sb.append("yes").append('\n');
-            }else{
+            } else {
                 sb.append("no").append('\n');
             }
         }
